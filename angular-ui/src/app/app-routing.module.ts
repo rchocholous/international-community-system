4682c730410e198f02e31fb63622545587242d97
chochy <radek.chocholous@gmail.com>
2020-08-05 23:07:07 +0200

    Angular UI add empty components and routing between them

--- angular-ui/src/app/app-routing.module.ts
+++ angular-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EventsOverviewComponent} from "./events-overview/events-overview.component";
+import {EventDetailComponent} from "./event-detail/event-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: EventsOverviewComponent},
+  { path: 'detail', component: EventDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EventsOverviewComponent } from './events-overview/events-overview.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsOverviewComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
